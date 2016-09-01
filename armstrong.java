5e29e538d0128ef92e9b2dac5fd5375005f3f4fb
ezhaprabha <ezhaprabhaperumal@gmail.com>
2016-09-01 19:24:27 +0530

    Add files via upload

---
+++ armstrong.java
@@ -0,0 +1,23 @@
+
+public class ArmstrongFinder {	
+ public static void main(String[] args) {
+
+	        int starting_number = 1;
+
+	        int ending_number = 99999;
+	        for (int i = starting_number; i <= ending_number; i++) {
+
+	            if (isArmstrong(i)) {
+
+	                System.out.println(i + " is an Armstrong number!");
+
+	            } else {
+
+	                 
+	            }
+
+	        }
+
+	    }
+
+	 
