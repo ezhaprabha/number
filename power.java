e9ec337a14bc03d4f785acc2441af97800266902
ezhaprabha <ezhaprabhaperumal@gmail.com>
2016-09-01 19:00:52 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,12 @@
+ 
+public class FindPowerExample {
+ 
+  public static void main(String[] args) {
+   Scanner s=new Scanner(System.in);
+   Integer a=s.nextInt();
+   Integer b=s.nextInt();
+   
+   System.out.println(Math.pow(a,b));
+  }
+}
+ 
